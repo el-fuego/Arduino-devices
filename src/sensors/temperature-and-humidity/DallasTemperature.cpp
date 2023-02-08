cf588a7f1730a9fbfd50cb6f3c2512e3c0b1f5dd
el-fuego <elfuegoyp@gmail.com>
2023-02-08 22:16:00 +0200

    Add DallasTemperature sensor

---
+++ src/sensors/temperature-and-humidity/DallasTemperature.cpp
@@ -0,0 +1,30 @@
+#include "./DallasTemperature.h"
+
+DallasTemperature_Sensor::DallasTemperature_Sensor(OneWire &_oneWire, uint8_t &_address) :
+  oneWire(_oneWire),
+  address(_address)
+  {};
+DallasTemperature_Sensor::DallasTemperature_Sensor(OneWire &_oneWire, uint8_t &_address, unsigned char _resolution) :
+  oneWire(_oneWire),
+  address(_address),
+  resolution(_resolution)
+  {};
+
+void DallasTemperature_Sensor::init() {
+  driver.begin();
+  driver.setWaitForConversion(false);
+
+  driver.setResolution(address, resolution);
+  driver.requestTemperaturesByAddress(address);
+};
+
+void DallasTemperature_Sensor::update() {
+  if (!canUpdate()) {
+    return;
+  }
+
+  updatedAt = millis();
+  temperature = static_cast<int>(driver.getTempC(address));
+  driver.requestTemperaturesByAddress(address);
+};
+
---
+++ src/sensors/temperature-and-humidity/DallasTemperature.h
@@ -0,0 +1,24 @@
+#ifndef DHT11_SENSOR_H
+#define DHT11_SENSOR_H
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+/**
+* Temperature and Humidity sensor
+**/
+class DallasTemperature_Sensor: public BaseDHT {
+  private:
+    DallasTemperature driver;
+    OneWire *oneWire;
+    uint8_t* address;
+    // 9 provide smallest request time, but resolution at 0.5*C, so using closest yet slower
+    unsigned char resolution = 10;
+  public:
+    DallasTemperature_Sensor(OneWire &_oneWire, uint8_t &_address);
+    DallasTemperature_Sensor(OneWire &_oneWire, uint8_t &_address, unsigned char _resolution);
+    void init();
+    void update();
+};
+
+#endif
